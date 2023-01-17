450040e640676d13675f6edb4b32939dbb60a9d5
Henrique Silveira <henrique.silveira@tech4h.com.br>
2023-01-16 22:29:03 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toogleMode() {
+  const html = document.documentElement
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatarLight.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
